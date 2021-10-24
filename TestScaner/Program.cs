bcd6f38d846fa2c2ab76896e5dfc9d44cf516841
Artikov Umid <umiddeveloper@gmail.com>
2021-10-24 12:42:11 +0500

    Fixed

---
+++ TestScaner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NaitonGPS.API;
+using ZebraBarcodeScannerSDK;
+
+namespace TestScaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SdkHandler.EnableBluetoothScannerDiscovery();
+            SdkHandler.SetSTCEnabledState(true);
+            SdkHandler.BarcodeDataEvent += BarcodeDataReceivedEvent;            
+            Console.ReadLine();
+        }
+
+        private static void BarcodeDataReceivedEvent(BarcodeData barcodeData, int scannerID)
+        {
+            Console.WriteLine(scannerID);
+            Console.WriteLine(barcodeData.ToString());
+        }
+    }
+}
