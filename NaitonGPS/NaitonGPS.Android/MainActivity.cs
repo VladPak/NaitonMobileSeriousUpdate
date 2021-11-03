932951ce6a8856875fc47c6241d670d1faab87a8
Khikmat <rahimov.hikmat@gmail.com>
2021-11-03 18:49:08 +0500

    to get a device data

--- NaitonGPS/NaitonGPS.Android/MainActivity.cs
+++ NaitonGPS/NaitonGPS.Android/MainActivity.cs
@@ -10,58 +10,72 @@ using Newtonsoft.Json;
 
 namespace NaitonGPS.Droid
 {
-    [Activity(Label = "Naiton", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-            //Removes status bar at the very top (time, battery level, network)
-            Window.AddFlags(WindowManagerFlags.Fullscreen);
-            Window.ClearFlags(WindowManagerFlags.ForceNotFullscreen);
+	[Activity(Label = "Naiton", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			//Removes status bar at the very top (time, battery level, network)
+			Window.AddFlags(WindowManagerFlags.Fullscreen);
+			Window.ClearFlags(WindowManagerFlags.ForceNotFullscreen);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            PhantomLib.Droid.Effects.Effects.Init();
-            ZXing.Net.Mobile.Forms.Android.Platform.Init(); 
-            ZXing.Mobile.MobileBarcodeScanner.Initialize(Application);
-
-
-            LoadApplication(new App());
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-
-        public override bool OnKeyMultiple([GeneratedEnum] Keycode keyCode, int repeatCount, KeyEvent e)
-        {
-            return base.OnKeyMultiple(keyCode, repeatCount, e);
-        }
+			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+			global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+			PhantomLib.Droid.Effects.Effects.Init();
+			ZXing.Net.Mobile.Forms.Android.Platform.Init();
+			ZXing.Mobile.MobileBarcodeScanner.Initialize(Application);
 
 
-        public override bool OnKeyUp([GeneratedEnum] Keycode keyCode, KeyEvent e)
-        {
-            App.Current.MainPage.DisplayAlert("Info 1", $"{(JsonConvert.SerializeObject(e))}", "Ok");
-            App.Current.MainPage.DisplayAlert("Info 2", $"{keyCode.ToString()}", "Ok");
+			LoadApplication(new App());
+		}
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+		{
+			Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            if ((int)keyCode == 10036 || keyCode==Keycode.Enter)
-            {
-                //SearchBar searchBar = FindViewById<SearchBar>(Resource.Id.searchT);
-                //if (e.Characters.Length > 5)
-                //    {
-                //        m_barcodeValue = new string(m_barcode.ToArray());
-                //        m_barcode.Clear();
-                //        CCDScanned(m_barcodeValue);
-                //    }
-                App.Current.MainPage.DisplayAlert("Info 3", $"{(e.Characters ?? "").ToString()}", "Ok");
-            }
+			global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            return base.OnKeyUp(keyCode, e);
-        }
-    }
+			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+
+		public override bool OnKeyMultiple([GeneratedEnum] Keycode keyCode, int repeatCount, KeyEvent e)
+		{
+			return base.OnKeyMultiple(keyCode, repeatCount, e);
+		}
+
+
+		public override bool OnKeyUp([GeneratedEnum] Keycode keyCode, KeyEvent e)
+		{
+			try
+			{
+				if (e.Device != null)
+					App.Current.MainPage.DisplayAlert("Device info", $"{e.Device}", "Ok");
+
+				App.Current.MainPage.DisplayAlert("Device Id", $"{e.DeviceId}", "Ok");
+				App.Current.MainPage.DisplayAlert("Event time", $"{e.EventTime}", "Ok");
+				App.Current.MainPage.DisplayAlert("Scan code", $"{e.ScanCode}", "Ok");
+				App.Current.MainPage.DisplayAlert("Source", $"{e.Source}", "Ok");
+				App.Current.MainPage.DisplayAlert("Info 2", $"{keyCode}", "Ok");
+
+				//e.Device.KeyCharacterMap
+
+				if ((int)keyCode == 10036 || keyCode == Keycode.Enter)
+				{
+					//SearchBar searchBar = FindViewById<SearchBar>(Resource.Id.searchT);
+					//if (e.Characters.Length > 5)
+					//    {
+					//        m_barcodeValue = new string(m_barcode.ToArray());
+					//        m_barcode.Clear();
+					//        CCDScanned(m_barcodeValue);
+					//    }
+					App.Current.MainPage.DisplayAlert("Info 3", $"I'm in", "Ok");
+				}
+			}
+			catch (Exception ex)
+			{
+				App.Current.MainPage.DisplayAlert("Error", $"{ex.Message}", "Ok");
+			}
+			return base.OnKeyUp(keyCode, e);
+		}
+	}
 }
